a7a2d02a9709280aefe4877965b71359295dc5ce
fajarnr <fajar1700018007@webmail.uad.ac.id>
2025-07-30 22:13:06 +0700

    copycat baru

--- resources/views/copycat.blade.php
+++ resources/views/copycat.blade.php
@@ -3,8 +3,8 @@
 @section('container')
 
 <div class="right-copycat">
-    <main class="main">
-        <section id="faq1" class="faq-section">
+    <main class="main-copycat">
+        <section id="faq1" class="faq-section-copycat">
           <p>I’m Fajar, a Web Developer and Data Analyst with a strong interest and experience in building web applications and performing data analysis and visualization to support data-driven decision-making.</p>
         </section>
     </main>
