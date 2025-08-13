e9eead08d0c8665b161868860f6be3e3dc1840fa
fajarnr <fajar1700018007@webmail.uad.ac.id>
2025-08-13 20:06:58 +0700

    tambahan copycat dua

--- resources/views/copycat.blade.php
+++ resources/views/copycat.blade.php
@@ -4,8 +4,14 @@
 
 <div class="right-copycat">
     <main class="main-copycat">
-        <section id="faq1" class="faq-section-copycat">
+        <section id="cc2" class="section-copycat">
+          <h6>Copycat</h6>
+        </section>
+        <section id="cc2" class="section-copycat">
           <p>I’m Fajar, a Web Developer and Data Analyst with a strong interest and experience in building web applications and performing data analysis and visualization to support data-driven decision-making.</p>
+        </section>
+        <section id="cc2" class="section-copycat">
+          <p>Junior–Mid Level Web Developer with foundational to intermediate skills in web development and programming. Proficient in HTML, CSS, and JavaScript, and currently expanding expertise in frameworks such as React and Laravel. Highly motivated to learn new technologies, build real-world projects, and continuously improve coding practices. Ready to grow and contribute to development teams with a strong learning mindset.</p>
         </section>
     </main>
 </div>
